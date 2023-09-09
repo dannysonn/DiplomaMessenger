fdc285e0d42bec3e0f9795ea4ee6ae76bccc1205
Danila Kryuchkov <dannyson5@bk.ru>
2023-09-09 19:05:55 +0300

    feature/Chat-page refactoring css

--- src/pages/Chats/Chats.tsx
+++ src/pages/Chats/Chats.tsx
@@ -8,27 +8,27 @@ function Chats() {
     <main className={styles.Chats}>
       <div className={styles.Chats__container}>
         <aside className={styles.Chats__sidebar}>
-          <header className={styles.Chats_sidebar__header}>
-            <h2 className={styles.Chats_sidebar__title}>Chats</h2>
+          <header className={styles["Chats-sidebar__header"]}>
+            <h2 className={styles["Chats-sidebar__title"]}>Chats</h2>
             <form action="">
               {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
               <button
-                className={styles.Chats_sidebar__add_user}
+                className={styles["Chats-sidebar__add-user"]}
                 type="submit"
               />
             </form>
           </header>
           <form action="" className="Chats-sidebar__search">
-            <div className={styles.Chats_sidebar__search_input_container}>
+            <div className={styles["Chats-sidebar__search-input-container"]}>
               <Input
-                inputClass={styles.Chats_sidebar__search_input}
+                inputClass={styles["Chats-sidebar__search-input"]}
                 type="text"
                 id="search-input"
                 placeholder="Search chat"
               />
             </div>
           </form>
-          <div className={styles.Chats_sidebar__wrapper}>
+          <div className={styles["Chats-sidebar__wrapper"]}>
             <Chat />
             <Chat />
             <Chat />
@@ -39,8 +39,11 @@ function Chats() {
             <Chat />
           </div>
 
-          <footer className={styles.Chats_sidebar__footer}>
-            <a href="/profile" className="Chats-sidebar__profile-link">
+          <footer className={styles["Chats-sidebar__footer"]}>
+            <a
+              href="/profile"
+              className={styles["Chats-sidebar__profile-link"]}
+            >
               <img src="" alt="" />
             </a>
             <form action="">
