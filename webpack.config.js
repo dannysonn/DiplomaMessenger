2de12de261ed9afc83015e12fb597e90d3510f53
dannysonn <dannyson5@bk.ru>
2024-01-27 06:03:09 +0300

    пытаюсь задеплоить

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,7 @@ module.exports = (env) => {
     plugins: [
       new HtmlWebpackPlugin({
         template: "../index.html",
+        publicPath: '/DiplomaMessenger/',
       }),
       new ESLintPlugin(),
     ],
